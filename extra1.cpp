76888053d37a1e8d18606cfc3854848224ee5b33
Ibrahim Safi <35405069+SheptheSheep1@users.noreply.github.com>
2025-11-10 14:22:02 -0700

    initial stuff

---
+++ extra1.cpp
@@ -0,0 +1,29 @@
+/* Encoder Library - Basic Example
+ * http://www.pjrc.com/teensy/td_libs_Encoder.html
+ *
+ * This example code is in the public domain.
+ */
+#define ENCODER_DO_NOT_USE_INTERRUPTS
+#include <Encoder.h>
+
+// Change these two numbers to the pins connected to your encoder.
+//   Best Performance: both pins have interrupt capability
+//   Good Performance: only the first pin has interrupt capability
+//   Low Performance:  neither pin has interrupt capability
+Encoder myEnc(15, 16);
+//   avoid using pins with LEDs attached
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Basic Encoder Test:");
+}
+
+long oldPosition  = -999;
+
+void loop() {
+  long newPosition = myEnc.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
+  }
+}
---
+++ src/main.cpp
@@ -0,0 +1,60 @@
+
+//A delay of 1000 Microseconds is Full Reverse
+//A delay of 1000 to 1460 Microseconds is Proportional Reverse
+//A delay of 1460 to 1540 Microseconds is neutral
+//A delay of 1540 to 2000 Microseconds is Proportional Forward
+//A delay of 2000 Microseconds is Full Forward
+
+#include <Servo.h>
+
+int backLeftPin = ;      //Back Left Motor pin
+volatile int backLeftSpeed = 1460; //Back Left Motor starting speed
+Servo backLeftMotor;           //Back Left Motor Servo Object
+
+int Speed = 1460;           //Starting speed for Serial communication
+bool change = false;
+
+void setup()
+{
+  // Tells each of the servo objects which pin it should output to
+  backLeftMotor.attach(backLeftPin);
+
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  // This code is used when controlling using serial
+
+  // FORMAT Example: fr 2000
+  // FORMAT Example: a 1500
+
+  // If you send the character "a" then all the motors will run at the provided speed
+  // fr -> front right
+  // fl -> front left
+  // br -> back right
+  // bl -> back left
+  if (Serial.available() > 0) {
+    char fb = Serial.read();
+    if (fb == 'a') {
+		Serial.println("got a");
+		Serial.println(backLeftSpeed, backLeftPin);
+    }
+    else {
+      char lr = Serial.read();
+      Speed = Serial.parseInt();
+      if (fb == 'b') {
+        if (lr == 'l') {
+          backLeftSpeed = Speed;
+		  //change = true;
+		  Serial.printf("wrote: %d to pin : %d", backLeftSpeed, backLeftPin);
+		}
+	  }
+	}
+    Serial.println(Speed);
+  }
+  
+  //This code creates the PWM signal on each pin based on the speed provided
+  backLeftMotor.writeMicroseconds(backLeftSpeed);           //Back left motor driver code
+  if(change == true){Serial.printf("wrote: %d to pin : %d", backLeftSpeed, backLeftPin);}
+}
